a8a27350b981b29078f7d0049b757b1db5100ab5
Dao Quang Phuong <daoquangphuong@gmail.com>
2026-07-23 20:10:51 +0700

    add script

--- youtube-mobile-embed/youtube-mobile-embed.user.js
+++ youtube-mobile-embed/youtube-mobile-embed.user.js
@@ -4,36 +4,72 @@
 // @version      2026-07-22
 // @description  Replace YouTube Player by Embed Player
 // @author       Dao Quang Phuong
-// @match        https://m.youtube.com/watch?v=*
+// @match        https://m.youtube.com/*
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=youtube.com
 // @downloadURL  https://raw.githubusercontent.com/daoquangphuong/tampermonkey/refs/heads/main/youtube-mobile-embed/youtube-mobile-embed.user.js
 // @updateURL    https://raw.githubusercontent.com/daoquangphuong/tampermonkey/refs/heads/main/youtube-mobile-embed/youtube-mobile-embed.user.js
+// @run-at       document-start
 // @grant        none
 // ==/UserScript==
 
 (function() {
-  const player = document.querySelector("#player-container-id");
-  const videoId = new URLSearchParams(window.location.search).get("v");
-  if (player && videoId) {
-    player.setAttribute(
-      "style",
-      `width: ${player.clientWidth}px; height: ${player.clientHeight}px`
-    );
-    player.textContent = "";
-    const iframe = document.createElement("iframe");
-    iframe.src = `https://www.youtube.com/embed/${videoId}?autoplay=1`;
-    iframe.width = "100%";
-    iframe.height = "100%";
-    iframe.frameBorder = "0";
-    iframe.allow =
-      "accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share";
-    iframe.allowFullscreen = true;
-    player.appendChild(iframe);
-  }
-  setInterval(() => {
-    const newVideoId = new URLSearchParams(window.location.search).get("v");
-    if (newVideoId !== videoId) {
-      window.location.reload();
+  const main = () => {
+    const video = document.querySelector('#player-container-id video');
+    if (video) {
+      video.pause();
     }
-  }, 1000);
+    const pause = document.querySelector(
+      'button.icon-button.player-control-play-pause-icon[aria-label*=Pause]'
+    );
+    if (pause) {
+      pause.click();
+    }
+
+    const videoId = new URLSearchParams(window.location.search).get('v');
+
+    if (videoId) {
+      const container = document.querySelector(
+        '#app .player-size.player-placeholder'
+      );
+      if (container) {
+        const src = `https://www.youtube.com/embed/${videoId}?autoplay=1`;
+        const oldIframe = container.querySelector('iframe');
+        if (oldIframe && oldIframe.src === src) {
+          return;
+        }
+        if (oldIframe) {
+          oldIframe.remove();
+        }
+        const iframe = document.createElement('iframe');
+        iframe.src = src;
+        iframe.width = '100%';
+        iframe.height = '100%';
+        iframe.style =
+          'position: absolute;top: 0;left: 0;z-index: 1;iframe.frameBorder = "0";border:none;';
+        iframe.allow =
+          'accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share';
+        iframe.allowFullscreen = true;
+        container.prepend(iframe);
+      }
+    }
+  };
+
+  const loop = () => {
+    main();
+    setTimeout(loop, 1000);
+  };
+
+  const observe = new window.MutationObserver(() => {
+    const app = document.querySelector('#player-container-id');
+    if (app) {
+      app.setAttribute('style', 'display: none;');
+    }
+    observe.disconnect();
+    loop();
+  });
+
+  observe.observe(document.documentElement, {
+    childList: true,
+    subtree: true,
+  });
 })();
